0ceb3275400796b54aa93eb13b0f327a820010db
PrakashGee <50893390+PrakashGee@users.noreply.github.com>
2019-05-23 03:24:53 -0700

    Update leap.java

--- leap.java
+++ leap.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class leap {
 public static void main(String[] args) {
 Scanner sc=new Scanner(System.in);
-System.out.print("Enter any year:");
 int year = sc.nextInt();
 boolean leap=false;
  if(year % 4 == 0)
