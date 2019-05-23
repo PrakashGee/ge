c3919d4fdf318702d3dca92b6b917d237d265db1
PrakashGee <50893390+PrakashGee@users.noreply.github.com>
2019-05-23 09:51:02 -0700

    Create Armg.java

---
+++ Armg.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Armg{  
+public static void main(String[] args)  
+{
+Scanner sc=new Scanner(System.in);
+int N=sc.nextInt();
+int c=0,a,temp;    
+temp=N;  
+while(N>0 && N<100000)  
+{  
+a=N%10;  
+N=N/10;  
+c=c+(a*a*a);  
+}  
+if(temp==c)  
+System.out.println("yes");   
+else  
+System.out.println("No");   
+}  
+}  
