128e0a4d1e869c9a6c241c55d4c6e96e1f8ed870
PrakashGee <50893390+PrakashGee@users.noreply.github.com>
2019-05-23 21:17:36 -0700

    Create mid.java

---
+++ mid.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class mid{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+if(a[0]>a[i])
+{
+System.out.print(a[i]);
+ a[i]=a[0];
+a[0]=a[i];
+System.out.print(a[i]);
+}
+else
+System.out.print(a[i]);
+}
+int m=n/2;
+System.out.print(m+1);
+}
+}
