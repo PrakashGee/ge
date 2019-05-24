faedec8c4b00f5f28963b3d8bfadf4c0beedfaff
PrakashGee <50893390+PrakashGee@users.noreply.github.com>
2019-05-23 21:05:55 -0700

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class sort{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=1;i<n;i++)
+{
+if(a[0]>a[i])
+{
+System.out.print(a[i]);
+ a[i]=a[0];
+a[0]=a[i];
+System.out.print(a[i]);
+}
+else
+System.out.print(a[i]);
+}
+}
+}
