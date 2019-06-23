bed14b86de3ae71c26b53e7e0d63ea5ebe767287
PrakashGee <50893390+PrakashGee@users.noreply.github.com>
2019-06-23 02:47:10 -0700

    Create fib.java

---
+++ fib.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class fib{
+public static void main(String[] args) {
+int n=5,f1=0,f2=1;
+for(int i=1;i<=n;++i)
+ {
+System.out.print(f1);
+int sum = f1 + f2;
+f1=f2;
+f2=sum;
+ } }
+}
