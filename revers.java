b179f7e8883c65e02464e3d6510fed848992ada1
PrakashGee <50893390+PrakashGee@users.noreply.github.com>
2019-06-23 23:23:04 -0700

    Update revers.java

--- revers.java
+++ revers.java
@@ -3,7 +3,6 @@ public class revers{
 public static void main(String[] args){
 Scanner read = new Scanner(System.in);
 String str = read.nextLine();
-StringBuilder sb = new StringBuilder();
 for(int i = str.length() - 1; i >= 0; i--)
 {
 char c=str.charAt(i);
