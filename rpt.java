16f8e1b60f1bc1ea0db2d69558e1695e914a8c23
PrakashGee <50893390+PrakashGee@users.noreply.github.com>
2019-06-24 21:44:01 -0700

    Create rpt.java

---
+++ rpt.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class s {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int n1=sc.nextInt();
+int arr[]=new int[n1];
+for(int i=0;i<n1;i++)
+{
+arr[i]=sc.nextInt();
+}
+int n = arr.length; 
+for (int i = 0; i < n; i++) { 
+int j; 
+for (j = 0; j < n; j++) 
+if (i == j && arr[i] == arr[j]) 
+System.out.print(arr[i]); 
+break; 
+
+} 
+}
+} 
+  
+   
